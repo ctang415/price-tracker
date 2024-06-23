02ac3829d22360f05e1f3d75379287c51c998e6f
ctang415 <carrietang117@gmail.com>
2024-06-23 16:07:56 -0400

    Updated visual display for pages and input type for website adding

--- vite-project/src/components/footer.jsx
+++ vite-project/src/components/footer.jsx
@@ -1,4 +1,15 @@
 const Footer = ({lastUpdated, allPages, sortText, sort, sortBy, search, retrieveProducts, setPage, page}) => {
+
+    function setBold(y) {
+        const pageNums = document.querySelectorAll("li[class^='text-xxs cursor-pointer hover:scale-125 hover:font-bold']");
+        for (let x = 0; x < pageNums.length; x++) {
+            if (pageNums[x].textContent == y) {
+                pageNums[x].style.fontWeight = '900';
+            } else {
+                pageNums[x].style.fontWeight = '500';
+            }
+        }
+    }
     
     return (
         <div className="flex flex-col">
@@ -7,7 +18,7 @@ const Footer = ({lastUpdated, allPages, sortText, sort, sortBy, search, retrieve
                 for (let i = 1; i < allPages+1; i++) {
                     arr.push(
                         <ul key={i}>
-                            <li onClick={sort || search ? () => sortBy(sortText, i) : () => { retrieveProducts(i); setPage(i); }} className={'text-xxs cursor-pointer hover:scale-125 hover:font-bold'}>{i}</li>
+                            <li onClick={sort || search ? () => { sortBy(sortText, i); setBold(i) } : () => { retrieveProducts(i); setPage(i); setBold(i) }} className={ i == 1 ? 'text-xxs cursor-pointer hover:scale-125 hover:font-bold font-bold': 'text-xxs cursor-pointer hover:scale-125 hover:font-bold'}>{i}</li>
                         </ul>
                     );
                 }
